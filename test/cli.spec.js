e7d46af0044de50b14b69f114f886c6919d1260e
Ilari Aarnio <ilari.aarnio@futurice.com>
2019-08-28 09:55:01 +0300

    Replace dev script with start script

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -1,12 +1,12 @@
-const CliTest = require("command-line-test");
+const CliTest = require('command-line-test');
 
-describe("test", function() {
-  it("build and run should be ok", async done => {
+describe('test', function() {
+  it('build and run should be ok', async done => {
     const cliTest = new CliTest();
-    const res = await cliTest.exec("npm run dev");
+    const res = await cliTest.exec('npm run start');
 
     expect(res.error).toBeNull();
-    expect(res.stderr).toMatch("");
+    expect(res.stderr).toMatch('');
 
     expect(res.stdout).toMatch(/https:\/\/github.com\/iaarnio/);
     expect(res.stdout).toMatch(/npx @aarnila\/card/);
